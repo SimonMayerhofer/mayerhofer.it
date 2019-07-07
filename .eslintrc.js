50caebec3e0c953f7add19144ccc51e00afde027
Simon Mayerhofer <git@maysi.de>
2019-07-08 00:50:52 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	globals: {
+		__PATH_PREFIX__: true,
+	},
+	"env": {
+		"es6": true,
+		"browser": true
+	},
+	"extends": ["airbnb", "prettier", "prettier/react"],
+	"plugins": ["prettier"],
+	"rules": {
+		"no-console": 1,
+		"no-unused-vars": 1,
+		"prettier/prettier": 2,
+		"react/jsx-indent": [2, "tab"],
+		"react/jsx-indent-props": [2, "tab"],
+		"react/jsx-filename-extension": 0,
+		"no-prototype-builtins": 0,
+		"react/prop-types": 0, // disable prop-type warnings and errors.
+		"react/forbid-prop-types": 0
+	},
+	"parser": "babel-eslint",
+	"parserOptions": {
+		"sourceType": "module",
+		"allowImportExportEverywhere": false,
+		"codeFrame": false
+	}
+};
