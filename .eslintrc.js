8ff6c6da05f47acef64f77718bd20fe92d8b1750
Simon Mayerhofer <git@maysi.de>
2019-07-08 01:12:35 +0200

    Add prop-types dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,8 @@ module.exports = {
 		"react/jsx-filename-extension": 0,
 		"no-prototype-builtins": 0,
 		"react/prop-types": 0, // disable prop-type warnings and errors.
-		"react/forbid-prop-types": 0
+		"react/forbid-prop-types": 0,
+		"react/no-danger": 0,
 	},
 	"parser": "babel-eslint",
 	"parserOptions": {
