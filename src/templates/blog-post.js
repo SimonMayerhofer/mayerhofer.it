e276fd15f4394458048a394881fd3d31339d1d80
Simon Mayerhofer <git@maysi.de>
2019-07-08 17:09:33 +0200

    Remove typography.js and all related dependencies

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,7 +4,6 @@ import { Link, graphql } from 'gatsby';
 import Bio from '../components/bio';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import { rhythm, scale } from '../utils/typography';
 
 const BlogPostTemplate = (data, pageContext, location) => {
 	const post = data.markdownRemark;
@@ -17,40 +16,17 @@ const BlogPostTemplate = (data, pageContext, location) => {
 				title={post.frontmatter.title}
 				description={post.frontmatter.description || post.excerpt}
 			/>
-			<h1
-				style={{
-					marginTop: rhythm(1),
-					marginBottom: 0,
-				}}
-			>
-				{post.frontmatter.title}
-			</h1>
-			<p
-				style={{
-					...scale(-1 / 5),
-					display: `block`,
-					marginBottom: rhythm(1),
-				}}
-			>
-				{post.frontmatter.date}
-			</p>
+			<h1>{post.frontmatter.title}</h1>
+
+			<p>{post.frontmatter.date}</p>
+
 			<div dangerouslySetInnerHTML={{ __html: post.html }} />
-			<hr
-				style={{
-					marginBottom: rhythm(1),
-				}}
-			/>
+
+			<hr />
+
 			<Bio />
 
-			<ul
-				style={{
-					display: `flex`,
-					flexWrap: `wrap`,
-					justifyContent: `space-between`,
-					listStyle: `none`,
-					padding: 0,
-				}}
-			>
+			<ul>
 				<li>
 					{previous && (
 						<Link to={previous.fields.slug} rel="prev">
