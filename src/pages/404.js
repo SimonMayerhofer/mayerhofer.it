748dae5d0e5a2441182ec8a5f42856953361f67d
Simon Mayerhofer <git@maysi.de>
2019-07-09 18:12:14 +0200

    Make components uppercase

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-import Layout from '../components/layout';
-import SEO from '../components/seo';
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class NotFoundPage extends React.Component {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Link, graphql } from 'gatsby';
 
-import Bio from '../components/bio';
-import Layout from '../components/layout';
-import SEO from '../components/seo';
+import Bio from '../components/Bio';
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class BlogIndex extends React.Component {
