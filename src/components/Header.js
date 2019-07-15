4b71986e677611f2a6771bb2cd69ff26e4852bc2
Simon Mayerhofer <git@maysi.de>
2019-07-15 13:09:55 +0200

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useStaticQuery, graphql, Link } from 'gatsby';
+import Image from 'gatsby-image';
+import { Location } from '@reach/router';
+import { isRootPath } from '../utils/path';
+
+import logo from '../assets/logo-white.svg';
+import './Header.scss';
+
+const Header = () => {
+	const { site, avatar } = useStaticQuery(graphql`
+		query HeaderQuery {
+			avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+				childImageSharp {
+					fluid(maxWidth: 175, maxHeight: 175) {
+						...GatsbyImageSharpFluid_withWebp
+					}
+				}
+			}
+			site {
+				siteMetadata {
+					author
+				}
+			}
+		}
+	`);
+
+	return (
+		<Location>
+			{({ location }) => {
+				const { author } = site.siteMetadata;
+				const isHome = isRootPath(location.pathname);
+				const style = isHome ? 'Header--home' : 'Header--compact';
+
+				return (
+					<header className={`Header ${style}`} role="banner">
+						<Link to="/">
+							<img className="Header__logo" src={logo} alt="Logo" />
+						</Link>
+
+						{isHome ? (
+							<React.Fragment>
+								<Image
+									className="Header__avatar"
+									fluid={avatar.childImageSharp.fluid}
+									alt={author}
+								/>
+
+								<h1 className="Header__title">{author}</h1>
+								<p className="Header__bio">
+									Web-Developer,
+									<br />
+									Co-Founder of{' '}
+									<a
+										href="https://mediadudes.lol?utm_source=website&utm_medium=referral&utm_campaign=simon&utm_content=bio_home"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<b>MEDIA</b>DUDES
+									</a>
+								</p>
+							</React.Fragment>
+						) : null}
+					</header>
+				);
+			}}
+		</Location>
+	);
+};
+
+export default Header;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,48 +1,32 @@
 import React from 'react';
-import { Link, useStaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 
-import { Location } from '@reach/router';
-import { isRootPath } from '../utils/path';
+import Header from './Header';
 
 const Layout = ({ children }) => {
 	const { site } = useStaticQuery(graphql`
 		query LayoutQuery {
 			site {
 				siteMetadata {
-					title
 					author
 				}
 			}
 		}
 	`);
 
-	const { title, author } = site.siteMetadata;
+	const { author } = site.siteMetadata;
 
 	return (
-		<Location>
-			{({ location }) => (
-				<React.Fragment>
-					<header className="header" role="banner">
-						{isRootPath(location.pathname) ? (
-							<h1>
-								<Link to="/">{title}</Link>
-							</h1>
-						) : (
-							<h3>
-								<Link to="/">{title}</Link>
-							</h3>
-						)}
-					</header>
+		<React.Fragment>
+			<Header />
 
-					{children}
+			{children}
 
-					<footer className="footer">
-						© {new Date().getFullYear()} {author}
-					</footer>
-				</React.Fragment>
-			)}
-		</Location>
+			<footer className="footer">
+				© {new Date().getFullYear()} {author}
+			</footer>
+		</React.Fragment>
 	);
 };
 
